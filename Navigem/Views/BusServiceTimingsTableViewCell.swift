d99c60c5999117c2606a9b40e2dc601e57484241
Ryan The <ryan.the.2006@gmail.com>
2020-12-01 17:36:51 +0800

    Add search placeholder

---
+++ Navigem/Views/BusServiceTimingsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  BusServiceTimingsTableViewCell.swift
+//  Navigem
+//
+//  Created by Ryan The on 1/12/20.
+//
+
+import UIKit
+
+class BusServiceTimingsTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        print("selected")
+    }
+    
+}
