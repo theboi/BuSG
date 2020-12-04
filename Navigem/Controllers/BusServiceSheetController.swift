15a7c9db86204927b46a180ed94703709deabce2
Ryan The <ryan.the.2006@gmail.com>
2020-12-04 17:56:50 +0800

    Fix missing data due to saving multiple entries

--- Navigem/Controllers/BusServiceSheetController.swift
+++ Navigem/Controllers/BusServiceSheetController.swift
@@ -9,23 +9,67 @@ import UIKit
 
 class BusServiceSheetController: SheetController {
 
+    var busService: BusService?
+    
+    lazy var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let trailingButton = UIButton(type: .close, primaryAction: UIAction(handler: { (action) in
             self.popSheet()
         }))
         headerView.trailingButton = trailingButton
+        
+        tableView.backgroundColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        contentView.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: tableView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: tableView.trailingAnchor),
+        ])
+        
+        tableView.register(BusServiceTimingsTableViewCell.self, forCellReuseIdentifier: K.identifiers.busService)
     }
     
-    init(for busServiceNo: String) {
+    init(for serviceNo: String) {
         super.init()
         
-        headerView.titleText = busServiceNo
-        headerView.detailText = "Singapore"
+        ApiProvider.shared.getBusService(for: serviceNo) {busService in
+            self.busService = busService
+            
+            self.headerView.titleText = busService?.serviceNo ?? "NULL"
+            self.headerView.detailText = busService?.destinationCode ?? "NULL"
+            
+            //print(busService?.busRoutes.allObjects as! [BusRoute])
+            //print((busService?.busRoutes.allObjects as! [BusRoute])[0].busStopCode)
+        }
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+}
 
+extension BusServiceSheetController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return busService!.busRoutes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.identifiers.busService)
+        cell?.backgroundColor = .clear
+        cell?.selectedBackgroundView = FillView(solidWith: (UIScreen.main.traitCollection.userInterfaceStyle == .dark ? UIColor.white : UIColor.black).withAlphaComponent(0.1))
+        cell?.textLabel?.text = (busService?.busRoutes.allObjects as! [BusRoute])[indexPath.row].busStopCode
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        present(BusStopSheetController(for: (busService?.busRoutes.allObjects as! [BusRoute])[indexPath.row].busStopCode), animated: true)
+    }
 }
