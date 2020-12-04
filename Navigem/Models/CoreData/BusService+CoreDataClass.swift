a9da517bb586cdd7a391ebb30c1403a4192b5da7
Ryan The <ryan.the.2006@gmail.com>
2020-12-04 20:05:19 +0800

    Add immediate fetch if relationship not mapped yet

--- Navigem/Models/CoreData/BusService+CoreDataClass.swift
+++ Navigem/Models/CoreData/BusService+CoreDataClass.swift
@@ -13,23 +13,35 @@ import CoreData
 public class BusService: NSManagedObject {
     
     public var busStops: [BusStop] {
-        let context = self.managedObjectContext!
-        let routeReq: NSFetchRequest<BusRoute> = BusRoute.fetchRequest()
-        routeReq.predicate = NSPredicate(format: "serviceNo == %@", serviceNo)
-        do {
-            let busRoutes = try context.fetch(routeReq)
-            return busRoutes.map({ (busRoute) -> BusStop in
-                let stopReq: NSFetchRequest<BusStop> = BusStop.fetchRequest()
-                stopReq.predicate = NSPredicate(format: "busStopCode == %@", busRoute.busStopCode)
-                do {
-                    return try context.fetch(stopReq).first!
-                } catch {
-                    fatalError(error.localizedDescription)
-                }
-            })
-        } catch {
-            fatalError(error.localizedDescription)
+        func fetchImmediately() -> [BusStop] {
+            let context = self.managedObjectContext!
+            let routeReq: NSFetchRequest<BusRoute> = BusRoute.fetchRequest()
+            routeReq.predicate = NSPredicate(format: "serviceNo == %@", serviceNo)
+            do {
+                let busRoutes = try context.fetch(routeReq)
+                return busRoutes.map({ (busRoute) -> BusStop in
+                    let stopReq: NSFetchRequest<BusStop> = BusStop.fetchRequest()
+                    stopReq.predicate = NSPredicate(format: "busStopCode == %@", busRoute.busStopCode)
+                    do {
+                        return try context.fetch(stopReq).first!
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                })
+            } catch {
+                fatalError(error.localizedDescription)
+            }
         }
+        
+        if let busRoutes = busRoutes, busRoutes.count > 0 {
+            var busStops: [BusStop] = []
+            for busRoute in (busRoutes.allObjects as! [BusRoute]) {
+                if let busStop = busRoute.busStop {
+                    busStops.append(busStop)
+                } else { return fetchImmediately() }
+            }
+            return busStops
+        } else { return fetchImmediately() }
     }
     
 }
