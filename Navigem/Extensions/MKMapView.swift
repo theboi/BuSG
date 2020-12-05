fd13bf231e8728f24deb95c3b115ef2836791347
Ryan The <ryan.the.2006@gmail.com>
2020-12-05 23:56:35 +0800

    Update mapview util func

--- Navigem/Extensions/MKMapView.swift
+++ Navigem/Extensions/MKMapView.swift
@@ -8,33 +8,15 @@
 import MapKit
 
 extension MKMapView {
-    /// when we call this function, we have already added the annotations to the map, and just want all of them to be displayed.
-    func fitAll() {
+    
+    /// Utility function to zoom view to fit all annotations provided
+    func fitAll(in annotations: [MKAnnotation]? = nil) {
+        let fitAnnotations = annotations ?? self.annotations
         var zoomRect = MKMapRect.null;
-        for annotation in annotations {
+        for annotation in fitAnnotations {
             let annotationPoint = MKMapPoint(annotation.coordinate)
             let pointRect = MKMapRect(x: annotationPoint.x, y: annotationPoint.y, width: 0.01, height: 0.01);
             zoomRect = zoomRect.union(pointRect);
-        }
-        setVisibleMapRect(zoomRect, edgePadding: K.mapView.edgePadding, animated: true)
-    }
-    
-    /// we call this function and give it the annotations we want added to the map. we display the annotations if necessary
-    func fitAll(in annotations: [MKAnnotation], andShow show: Bool) {
-        var zoomRect:MKMapRect  = MKMapRect.null
-        
-        for annotation in annotations {
-            let aPoint = MKMapPoint(annotation.coordinate)
-            let rect = MKMapRect(x: aPoint.x, y: aPoint.y, width: 0.1, height: 0.1)
-            
-            if zoomRect.isNull {
-                zoomRect = rect
-            } else {
-                zoomRect = zoomRect.union(rect)
-            }
-        }
-        if (show) {
-            addAnnotations(annotations)
         }
         setVisibleMapRect(zoomRect, edgePadding: K.mapView.edgePadding, animated: true)
     }
