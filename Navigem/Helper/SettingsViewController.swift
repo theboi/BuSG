11bf9c6c1d2a222fa39633473e7b38459100117c
Ryan The <ryan.the.2006@gmail.com>
2020-12-07 17:19:51 +0800

    Remove settings redundant closure calls

--- Navigem/Helper/SettingsViewController.swift
+++ Navigem/Helper/SettingsViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-typealias SettingsAction = () -> Void
-typealias SettingsList = () -> [[SettingsTableItem]]
+typealias SettingsTableList = [[SettingsTableItem]]
+
 struct SettingsTableItem {
     var title: String
     var image: UIImage?
@@ -17,41 +17,39 @@ struct SettingsTableItem {
     var accessoryView: UIView?
     var pushViewController: UIViewController?
     var presentViewController: UIViewController?
-    var action: SettingsAction?
+    var action: (() -> Void)?
 }
 
 class SettingsViewController: UITableViewController {
     
-    var listData: SettingsList!
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var listData: SettingsTableList!
     
-    var defaultList: SettingsList {
-        { () -> [[SettingsTableItem]] in
-            return [
-                [
-                    SettingsTableItem(title: "Privacy", presentViewController: { () -> UIAlertController in
-                        let privacyAlert = UIAlertController(title: "Privacy", message: "Your data will be kept private and confidential... hopefully...", preferredStyle: .alert)
-                        privacyAlert.addAction(UIAlertAction(title: "OK", style: .default))
-                        return privacyAlert
-                    }()),
-                ],
-                [
-//                    SettingsTableItem(title: "History", pushViewController: UIViewController()),
-                    SettingsTableItem(title: "Privacy", presentViewController: { () -> UIAlertController in
-                        let privacyAlert = UIAlertController(title: "Privacy", message: "Your data will be kept private and confidential... hopefully...", preferredStyle: .alert)
-                        privacyAlert.addAction(UIAlertAction(title: "OK", style: .default))
-                        return privacyAlert
-                    }()),
-                ],
-            ]
-        }
+    var defaultList: SettingsTableList {
+        [
+            [
+                SettingsTableItem(title: "Auto Refresh Bus Timing", accessoryView: UISwitch()),
+                SettingsTableItem(title: "Update Bus Data", accessoryView: UISwitch()),
+                
+            ],
+            [
+//                SettingsTableItem(title: "History", pushViewController: UIViewController()),
+                SettingsTableItem(title: "Share with a Friend", accessoryView: UISwitch()),
+                SettingsTableItem(title: "Rate on App Store", accessoryView: UISwitch()),
+                SettingsTableItem(title: "Open Sourced Repository", action: {
+                    if let url = URL(string: "https://github.com/theboi/BuSG") {
+                        UIApplication.shared.open(url, options: [:])
+                    }
+                }),
+                SettingsTableItem(title: "Report an Issue", accessoryView: UISwitch()),
+            ],
+        ]
     }
     
-    init(list: SettingsList? = nil) {
+    init(list: SettingsTableList? = nil) {
         super.init(nibName: nil, bundle: nil)
         self.title = "Settings"
         self.tableView = UITableView(frame: CGRect(), style: .insetGrouped)
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "settingsTableViewCell")
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: K.identifiers.settings)
         self.listData = list ?? defaultList
     }
     
@@ -66,17 +64,17 @@ class SettingsViewController: UITableViewController {
     // MARK: UITableViewDataSource
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return listData().count
+        return listData.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listData()[section].count
+        return listData[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cellData = listData()[indexPath.section][indexPath.row]
-        let cell = cellData.customCell ?? tableView.dequeueReusableCell(withIdentifier: "settingsTableViewCell", for: indexPath)
+        let cellData = listData[indexPath.section][indexPath.row]
+        let cell = cellData.customCell ?? tableView.dequeueReusableCell(withIdentifier: K.identifiers.settings, for: indexPath)
         
         cell.textLabel?.text = cellData.title
         if let accessoryView = cellData.accessoryView {
@@ -96,11 +94,11 @@ class SettingsViewController: UITableViewController {
     // MARK: UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return listData()[indexPath.section][indexPath.row].height ?? tableView.rowHeight
+        return listData[indexPath.section][indexPath.row].height ?? tableView.rowHeight
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cellData = listData()[indexPath.section][indexPath.row]
+        let cellData = listData[indexPath.section][indexPath.row]
         tableView.deselectRow(at: indexPath, animated: true)
         cellData.action?()
         if let nextViewController = cellData.pushViewController {
