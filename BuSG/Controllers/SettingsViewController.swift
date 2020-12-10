88653ca7b891dfc4c3e5b4c8c62401b20e1c9df5
Ryan The <ryan.the.2006@gmail.com>
2020-12-10 21:42:25 +0800

    Optimise header with custom view settings

--- BuSG/Controllers/SettingsViewController.swift
+++ BuSG/Controllers/SettingsViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 import StoreKit
 
-typealias SettingsTableList = [SettingsTableSection]
-
 struct SettingsTableItem {
     var title: String
     var image: UIImage?
@@ -31,48 +29,12 @@ struct SettingsTableSection {
 
 class SettingsViewController: UITableViewController {
     
-    var listData: SettingsTableList!
+    var tableData: [SettingsTableSection]!
     
-    var rootList: SettingsTableList {
-        [
-            SettingsTableSection(tableItems: [
-                SettingsTableItem(title: "Bus Data", pushViewController: SettingsViewController(list: [
-                    SettingsTableSection(tableItems: [
-                        SettingsTableItem(title: "Update Now", action: {
-                            // TODO
-                        })
-                    ]),
-                    SettingsTableSection(tableItems: [
-                        SettingsTableItem(title: "Once per week"),
-                        SettingsTableItem(title: "Once per month"),
-                        SettingsTableItem(title: "Never"),
-                    ], headerText: "Update Frequency", footerText: "")
-                ])),
-            ]),
-            SettingsTableSection(tableItems: [
-                //                SettingsTableItem(title: "History", pushViewController: UIViewController()),
-                SettingsTableItem(title: "Share with a Friend", presentViewController: {
-                    return UIActivityViewController(activityItems: ["Check out BuSG, a smart bus tracker app!"], applicationActivities: [])
-                }()),
-                SettingsTableItem(title: "Rate on App Store", urlString: "itms-apps://itunes.apple.com/app/\(Bundle.main.bundleIdentifier)"),
-                SettingsTableItem(title: "Open-Sourced Repository", urlString: "https://github.com/theboi/BuSG"),
-                SettingsTableItem(title: "Report an Issue", urlString: "https://github.com/theboi/BuSG"),
-            ]),
-        ]
-    }
-    
-    init(list: SettingsTableList? = nil) {
+    init(tableData: [SettingsTableSection] = []) {
         super.init(nibName: nil, bundle: nil)
-        title = "Settings"
-        
+        self.tableData = tableData
         tableView = UITableView(frame: CGRect(), style: .insetGrouped)
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .close, primaryAction: UIAction(handler: { _ in
-            self.dismiss(animated: true)
-        }))
-        
-        self.listData = list ?? rootList
-        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: K.identifiers.settingsCell)
         tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: K.identifiers.settingsHeader)
     }
@@ -81,31 +43,64 @@ class SettingsViewController: UITableViewController {
         super.init(coder: coder)
     }
     
-    @objc func authStateDidChange(notification: NSNotification) {
-        self.tableView.reloadData()
+    override func viewDidLoad() {
+        title = "Settings"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .close, primaryAction: UIAction(handler: { _ in
+            self.dismiss(animated: true)
+        }))
     }
     
     // MARK: UITableViewDataSource
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        listData[section].headerText
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let sectionData = tableData[section]
+        
+        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: K.identifiers.settingsHeader)!
+        
+        let label = UILabel()
+        view.addSubview(label)
+        label.text = sectionData.headerText
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -K.margin.large),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: K.margin.large+K.margin.small),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -K.margin.large-K.margin.small),
+        ])
+        
+        if let button = sectionData.headerTrailingButton {
+            view.addSubview(button)
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -K.margin.large),
+                button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -K.margin.large-K.margin.small),
+            ])
+        }
+        
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        tableData[section].headerText != nil ? 60 : 20
     }
     
     override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        listData[section].footerText
+        tableData[section].footerText
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return listData.count
+        return tableData.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listData[section].tableItems.count
+        return tableData[section].tableItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cellData = listData[indexPath.section].tableItems[indexPath.row]
+        let cellData = tableData[indexPath.section].tableItems[indexPath.row]
         let cell = cellData.customCell ?? tableView.dequeueReusableCell(withIdentifier: K.identifiers.settingsCell, for: indexPath)
         
         cell.textLabel?.text = cellData.title
@@ -128,11 +123,11 @@ class SettingsViewController: UITableViewController {
     // MARK: UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return listData[indexPath.section].tableItems[indexPath.row].height ?? K.cellHeight
+        return tableData[indexPath.section].tableItems[indexPath.row].height ?? K.cellHeight
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cellData = listData[indexPath.section].tableItems[indexPath.row]
+        let cellData = tableData[indexPath.section].tableItems[indexPath.row]
         tableView.deselectRow(at: indexPath, animated: true)
         cellData.action?()
         if let urlString = cellData.urlString {
