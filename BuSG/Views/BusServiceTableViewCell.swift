50dfaf5d917bba1a0b6071c88350635d2bb0d2cf
Xavier3372 <koh_jie_ming_xavier@s2019.ssts.edu.sg>
2020-12-13 15:21:08 +0800

    add cells

--- BuSG/Views/BusServiceTableViewCell.swift
+++ BuSG/Views/BusServiceTableViewCell.swift
@@ -1,20 +1,62 @@
 //
-//  BusServiceTimingsTableViewCell.swift
+//  BusStopTableViewCell.swift
 //  Navigem
 //
-//  Created by Ryan The on 1/12/20.
+//  Created by Ryan The on 4/12/20.
 //
 
 import UIKit
 
-class BusServiceTableViewCell: UITableViewCell {
+enum BusStopData {
+    case arr
+    case left
+    case mins(Int)
+}
 
+class BusServiceTableViewCell: UITableViewCell {
+    
+    public lazy var blockLabel = UILabel()
+    public lazy var streetLabel = UILabel()
+    public lazy var busStopCodeLabel = UILabel()
+    public lazy var blockImage = UIImageView()
+    public var stackView: UIStackView!
+    public lazy var errorLabel = UILabel()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+        
+        addSubview(blockLabel)
+        blockLabel.font = blockLabel.font.withSize(17)
+        blockLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            blockLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: K.margin.large),
+            blockLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            blockLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant:
+                                                -26)
+        ])
+        
+        addSubview(busStopCodeLabel)
+        busStopCodeLabel.font = busStopCodeLabel.font.withSize(12)
+        busStopCodeLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            busStopCodeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: K.margin.large),
+            busStopCodeLabel.topAnchor.constraint(equalTo: blockLabel.bottomAnchor, constant: 2)
+        ])
+
+        addSubview(streetLabel)
+        streetLabel.font = streetLabel.font.withSize(12)
+        streetLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            streetLabel.leadingAnchor.constraint(equalTo: busStopCodeLabel.trailingAnchor, constant: 7),
+            streetLabel.topAnchor.constraint(equalTo: blockLabel.bottomAnchor, constant: 2)
+        ])
+
+        
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
 }
